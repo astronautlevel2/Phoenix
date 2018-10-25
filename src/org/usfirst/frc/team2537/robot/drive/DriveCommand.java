482f4543ba9a63299665bec8b29453d9fe257b94
Alex Taber <astronautlevel2@users.noreply.github.com>
2018-10-25 18:50:24 -0400

    Update DriveCommand.java

--- src/org/usfirst/frc/team2537/robot/drive/DriveCommand.java
+++ src/org/usfirst/frc/team2537/robot/drive/DriveCommand.java
@@ -1,38 +1,28 @@
-package org.usfirst.frc.team2537.robot.drive;
-
-import org.usfirst.frc.team2537.robot.Robot;
-import org.usfirst.frc.team2537.robot.input.HumanInput;
-
-import edu.wpi.first.wpilibj.command.Command;
-
 public class DriveCommand extends Command {
-
+	
 	public DriveCommand() {
 		requires(Robot.driveSys);
 	}
-	
 	@Override
 	protected void initialize() {
-
+		// We don't need to initialize anything for this command
 	}
 	
 	@Override 
 	protected void execute() {
-		Robot.driveSys.setMotors(-HumanInput.rightJoystick.getRawAxis(1), Motor.RIGHT);
-		Robot.driveSys.setMotors(-HumanInput.leftJoystick.getRawAxis(1), Motor.LEFT);
-//		Robot.driveSys.fixingDrive();
+		Robot.driveSys.setMotors(.5); // sets both motors to 50% speed
 	}
 	
 	@Override
 	protected boolean isFinished() {
-		return false;
+		return false; // always runs
 	}
 	
 	protected void end() {
-		
+		Robot.driveSys.setMotors(0);
 	}
 	
 	protected void interrupted() {
-		
+		end(); // stop motors when finished		
 	}
 }
